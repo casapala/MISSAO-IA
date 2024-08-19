fa3db928154289dfa3b006f4381078b25be09397
casapala <110059349+casapala@users.noreply.github.com>
2024-08-19 08:19:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
+import {aleatorio} from "./aleatorio.js";
+import {perguntas} from "./perguntas.js";
+
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas = [
-    {
-        enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas que uma pessoa pode ter. Além disso, o chat também gera imagens e áudios hiper-realistas. Qual o seu primeiro pensamento?",
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: [
-                    "No início, ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade com que a tecnologia está avançando."
-                ]
-            },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
-                ]
-            }
-        ]
-    }
-];
+
 
 let atual = 0;
 
@@ -51,15 +34,14 @@ function mostraResultado(afirmacao) {
 // Inicializa a primeira pergunta
 mostraPergunta();
 
-function aleatorio (lista){
-    const posicao = Math.floor(Math.random())*lista.length;
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
 }
 
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
-    historiaFinal += afirmacoes +  " ";
+    historiaFinal += afirmacoes + '';
     atual++;
     mostraPergunta();
     }
-
